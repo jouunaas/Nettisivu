29dceae8a1a07061d20fce792b34388d75cb44d6
jonas.montonen <jonasmontonen@kamk.fi>
2024-10-03 11:03:42 +0300

    ds

--- api/login.js
+++ api/login.js
@@ -16,23 +16,23 @@ module.exports = async (req, res) => {
             const values = [username];
             const result = await pool.query(query, values);
 
-            if (result.rows.length > 0) {
+           // if (result.rows.length > 0) {
                 const user = result.rows[0];
                 const isMatch = await bcrypt.compare(password, user.password);
 
-                if (isMatch) {
+                //if (isMatch) {
                     const token = jwt.sign(
                         { userId: user.id },
                         process.env.JWT_SECRET || 'defaultsecret',
                         { expiresIn: '1h' }
                     );
                     res.status(200).json({ token });
-                } else {
-                    res.status(401).json({ message: 'Incorrect login details' });
-                }
-            } else {
-                res.status(401).json({ message: 'User not found' });
-            }
+                //} else {
+                //    res.status(401).json({ message: 'Incorrect login details' });
+                //}
+           // } else {
+            //    res.status(401).json({ message: 'User not found' });
+            //}
         } catch (error) {
             console.error('Error during login:', error);
             res.status(500).json({ message: 'Server error', error: error.message });
